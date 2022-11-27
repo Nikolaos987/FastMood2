01f4d52a9758e3ffd832ac0d8246d69961a8b860
Nikolaos987 <73761101+Nikolaos987@users.noreply.github.com>
2022-11-27 15:05:18 +0200

    added edit function for fullname of customer no.3

--- src/main/java/com/example/fastmood2/AccountController.java
+++ src/main/java/com/example/fastmood2/AccountController.java
@@ -66,22 +66,23 @@ public class AccountController {
 
 
     public void SaveFullnameButton_Pressed() throws IOException {
+        String call = "{CALL EDITUSER(?,?)}";
+        Connection connection = null;
         try {
             System.out.println("Trying to connect to database...");
-            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@192.168.6.21:1521:dblabs", "it185351", "Oreoskodikos_33");
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@192.168.6.21:1521:dblabs", "it185351", "Oreoskodikos_33");
             System.out.println("Connected to database");
 
 
-            String call = "{CALL EDITUSER(?,?)}";
-
             try (CallableStatement stmt = connection.prepareCall(call)) {
-                System.out.println("Fullname: "+ User.getFullname());
+                System.out.println("old Fullname: "+ User.getFullname());
                 System.out.println("CID: "+ User.getID());
+                System.out.println("new Fullname: "+ fullname.getText());
 
-//                stmt.setString(1, User.getFullname());
-//                stmt.setInt(2, User.getID());
-//                stmt.execute();
-//                System.out.println("\nCUSTOMER: fullname changed!");
+                stmt.setString(1, fullname.getText());
+                stmt.setInt(2, User.getID());
+                stmt.execute();
+                System.out.println("\nCUSTOMER: fullname changed!");
             }
 
         }catch (SQLException e) {
@@ -123,8 +124,6 @@ public class AccountController {
         }catch (Exception e) {
             System.out.println(e);
         }
-
-
 
     }
 
